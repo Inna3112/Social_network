0fab11d9c2c3ee4b4d5b0d89a0c1320c6d747298
Inna3112 <innula3112@gmail.com>
2021-05-25 18:52:03 +0300

    Update users-redusers.test.ts

--- src/redux/users-reducer.test.ts
+++ src/redux/users-reducer.test.ts
@@ -1,42 +1,10 @@
 import avaPost from "../assets/images/avaPost.png";
-import usersReducer, {followAC, unFollowAC, UsersStateType} from "./users-reducer";
+import usersReducer, {followAC, LocationType, setUsersAC, unFollowAC, UsersStateType} from "./users-reducer";
 
-test('followed should be true', () => {
-    const startState: UsersStateType = {
-        users: [
-        {
-            id: 1,
-            photoUrl: avaPost,
-            followed: true,
-            name: 'Anna',
-            status: 'I am happy',
-            location: {city: 'Kyiv', country: 'Ukraine'}
-        },
-        {
-            id: 2,
-            photoUrl: avaPost,
-            followed: false,
-            name: 'Inna',
-            status: 'I am sed',
-            location: {city: 'Kyiv', country: 'Ukraine'}
-        },
-        {
-            id: 3,
-            photoUrl: avaPost,
-            followed: true,
-            name: 'Max',
-            status: 'I am  too happy',
-            location: {city: 'Kyiv', country: 'Ukraine'}
-        }]}
-    const action = followAC(0)
-    const endState = usersReducer(startState, action)
+let initialState: UsersStateType = {users: []}
 
-    expect(endState.users[0].followed).toBe(true)
-
-})
-
-test('followed should be false', () => {
-    const startState: UsersStateType = {
+beforeEach(() => {
+    initialState = {
         users: [
             {
                 id: 1,
@@ -61,10 +29,46 @@ test('followed should be false', () => {
                 name: 'Max',
                 status: 'I am  too happy',
                 location: {city: 'Kyiv', country: 'Ukraine'}
-            }]}
-    const action = unFollowAC(0)
-    const endState = usersReducer(startState, action)
+            }]
+    }
+})
 
-    expect(endState.users[0].followed).toBe(false)
+test('followed should be true', () => {
+
+    const action = followAC(3)
+    const endState = usersReducer(initialState, action)
+
+    expect(endState.users[2].followed).toBe(true)
+
+})
+
+test('followed should be false', () => {
+
+    const action = unFollowAC(2)
+    const endState = usersReducer(initialState, action)
+
+    expect(endState.users[1].followed).toBe(false)
+
+})
+
+test('users should be set', () => {
+    const newUsers = [
+        {
+            id: 4,
+            photoUrl: avaPost,
+            followed: false,
+            name: 'Alex',
+            status: 'I.....',
+            location: {
+                city: 'Berlin',
+                country: 'Germany'
+            }
+        }
+    ]
+    const action = setUsersAC(newUsers)
+    const endState = usersReducer(initialState, action)
+
+    expect(endState.users.length).toBe(4)
+    expect(endState.users[3].id).toBe(4)
 
 })
